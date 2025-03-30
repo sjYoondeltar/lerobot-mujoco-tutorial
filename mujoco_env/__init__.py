ae5311972473f7494f70476116568a3aa5b62972
sjYoondeltar <dbsdns@naver.com>
2025-03-30 15:46:38 +0900

    [sjy] init fix

--- mujoco_env/__init__.py
+++ mujoco_env/__init__.py
@@ -1,1 +1,5 @@
-from .mujoco_parser import MuJoCoParserClass
+# mujoco_env package
+# This file makes the mujoco_env directory a Python package
+
+# Instead of immediately importing everything, we'll import modules only when needed
+# This prevents import errors during module discovery
